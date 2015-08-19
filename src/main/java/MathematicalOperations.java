00cd0ea4e6372fc8a8d4a305b42fd3cf9c58448c
KSedlak <krzysztof.sedlak@hotmail.com>
2015-08-19 09:14:52 +0200

    Add guava library to pom
    Add division operation

--- src/main/java/MathematicalOperations.java
+++ src/main/java/MathematicalOperations.java
@@ -1,4 +1,19 @@
+import java.math.RoundingMode;
 
+import com.google.common.math.*;
 public class MathematicalOperations {
+	
+	
+	
+	
+
+
+	public double division(int a, int b, boolean round) throws Exception {
+		if(round)
+		return IntMath.divide(a, b, RoundingMode.FLOOR);
+		
+		return a/b;
+	}
+	
 
 }
